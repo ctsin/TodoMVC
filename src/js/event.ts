571a91924166649a9854b8f49ff5670254dfe0b7
邢贵志 <xinggz@zhongan.com.cn>
2018-08-09 11:43:35 +0800

    完成事件总栈

--- src/js/event.ts
+++ src/js/event.ts
@@ -1,5 +1,23 @@
 export class Event {
+  private hub;
+
   constructor() {
-    console.log("EVENT");
+    this.hub = Object.create(null);
+  }
+
+  emit(event: string, data) {
+    (this.hub[event] || []).forEach(handler => handler(data));
+  }
+
+  on(event: string, handler) {
+    this.hub[event] = this.hub[event] || [];
+
+    this.hub[event].push(handler);
+  }
+
+  off(event: string, handler) {
+    const i = (this.hub[event] || []).findIndex(h => h === handler);
+
+    i > -1 && this.hub[event].splice(i, 1);
   }
 }
