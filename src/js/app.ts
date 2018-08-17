50402a37c3324c99e2e7d9a41ff895d3f8911c5b
邢贵志 <xinggz@zhongan.com.cn>
2018-08-17 17:25:46 +0800

    去除 onload 事件

--- src/js/app.ts
+++ src/js/app.ts
@@ -5,7 +5,4 @@ import { View } from "./view";
 import { Template } from "./template";
 import { Store } from "./store";
 
-window.onload = () => {
-  // 嵌——嵌——嵌——套——套——套——嵌——嵌——嵌
-  new Controller(new Model(new Store()), new View(new Template()));
-};
+new Controller(new Model(new Store()), new View(new Template()));
